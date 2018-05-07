1efe920c759e1a82eed20d64c8388cb44f83ae93
sgar38 <sgarg38@sapient.com>
2018-05-07 17:00:46 +0530

    'ok'

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res) {
 
 /* eslint-disable no-console */
 app.listen(port, function(err) {
-  if(err) {
+  if (err) {
     console.log(err);
   } else{
     open('http://localhost:' + port)
