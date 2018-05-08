1af1fe4e3b2d23af62390496945a642035653fc7
sgar38 <sgarg38@sapient.com>
2018-05-08 15:37:36 +0530

    'ok'

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,6 @@
 import path from 'path';
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+//import browserSyncWebpackPlugin from 'browser-sync-webpack-plugin';
 
 export default {
   debug: true,
@@ -11,9 +13,25 @@ export default {
   output: {
     path: path.resolve(__dirname, 'src'),
     publicPath: '/',
-    filename: 'srcBundle/bundle.js'
+    filename: 'bundle.js'
   },
-  plugins: [],
+  watch: true,
+  plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:4500/',
+      files: ['src']
+    },
+    // plugin options
+    {
+      // prevent BrowserSync from reloading the page
+      // and let Webpack Dev Server take care of this:: reload: false
+      reload: true
+    })
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
